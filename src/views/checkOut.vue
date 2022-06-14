2017c578a6da72229b7e44218220e6867acaa1fd
kolenguyen <70668231+kolenguyen@users.noreply.github.com>
2022-06-13 21:26:51 -0400

    connect to db and update ui

---
+++ src/views/checkOut.vue
@@ -0,0 +1,14 @@
+<template>
+    <checkOutComponent />
+</template>
+
+<script>
+//Note to future: This is not needed to do checkOutComponent.vue because checkOut.vue is not complicated;
+//This is better when have a vue component that is complicated
+import checkOutComponent from '../components/checkOutComponent.vue'
+export default {
+    components: {
+      checkOutComponent
+  },
+}
+</script>
